c99267544ff252795240b57b1a6fbda1b30dadea
Adrian Mlodzianowski <amlodzianowski@logicworks.net>
2016-11-28 17:04:58 -0500

    adding debug flag to AD installation

--- manifests/conf_forest.pp
+++ manifests/conf_forest.pp
@@ -74,7 +74,7 @@ class windows_ad::conf_forest (
         if ($installdns == 'yes'){
           # Deploy Server 2012 Active Directory
           exec { 'Config ADDS':
-            command     => "Start-Transcript C:\logicworks\ad-log.txt; Import-Module ADDSDeployment; Install-ADDSForest -Force -DomainName ${domainname} -DomainMode ${domainlevel} -DomainNetbiosName ${netbiosdomainname} -ForestMode ${forestlevel} -DatabasePath ${databasepath} -LogPath ${logpath} -SysvolPath ${sysvolpath} -SafeModeAdministratorPassword (convertto-securestring '${dsrmpassword}' -asplaintext -force) -InstallDns",
+            command     => "Start-Transcript C:\logicworks\ad-log.txt; Import-Module ADDSDeployment; Install-ADDSForest -Debug -Force -DomainName ${domainname} -DomainMode ${domainlevel} -DomainNetbiosName ${netbiosdomainname} -ForestMode ${forestlevel} -DatabasePath ${databasepath} -LogPath ${logpath} -SysvolPath ${sysvolpath} -SafeModeAdministratorPassword (convertto-securestring '${dsrmpassword}' -asplaintext -force) -InstallDns",
             provider    => powershell,
             onlyif      => "if((gwmi WIN32_ComputerSystem).Domain -eq \'${domainname}\'){exit 1}",
             timeout     => $timeout,
